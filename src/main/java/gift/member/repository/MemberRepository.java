39b29c1fa02589fe357cd37eb03c5cf0e22c672e
KWH <kmwh030@gmail.com>
2025-07-14 16:07:52 +0900

    feat: wishlist 코드 복사

---
+++ src/main/java/gift/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package gift.member.repository;
+
+import gift.member.entity.Member;
+import java.util.List;
+
+public interface MemberRepository {
+    void save(Member member);
+    Member findByEmail(String email);
+    List<Member> findAll();
+    Member findById(Long id);
+    void update(Member member);
+    void delete(Long id);
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE products (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  price INT NOT NULL,
+  image_url VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE members (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE wishlist (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  member_id BIGINT,
+  product_id BIGINT,
+  amount INT
+)
